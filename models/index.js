07eae7ec74d6ab130a44ae6eda5ad1235fa52388
jwkwon-715 <3r453.618@gmail.com>
2025-05-16 14:36:40 +0900

    모델 세팅

--- models/index.js
+++ models/index.js
@@ -9,6 +9,13 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
+config.define = {
+  freezeTableName: true,
+  charset: 'utf8',
+  collate: 'utf8_general_ci',
+  timestamps: false
+};
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -16,6 +23,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+// 모델 자동으로 등록
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -31,10 +39,142 @@ fs
     db[model.name] = model;
   });
 
+// 관계 설정
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
+});
+
+// 관계 명시 (명시적 관계 설정이 필요한 경우)
+db.Users.hasMany(db.UserCharacters, { foreignKey: 'user_id' });
+db.UserCharacters.belongsTo(db.Users, { foreignKey: 'user_id' });
+
+db.UserCharacters.hasMany(db.Inventory, { foreignKey: 'user_character_id' });
+db.Inventory.belongsTo(db.UserCharacters, { foreignKey: 'user_character_id', onDelete: 'CASCADE' });
+
+db.Items.hasMany(db.Inventory, { foreignKey: 'item_id' });
+db.Inventory.belongsTo(db.Items, { foreignKey: 'item_id', onDelete: 'CASCADE' });
+
+db.Curriculum.hasMany(db.Quiz, {
+  foreignKey: 'curriculum_id'
+});
+db.Quiz.belongsTo(db.Curriculum, {
+  foreignKey: 'curriculum_id'
+});
+
+db.Quiz.hasMany(db.QuizOption, {
+  foreignKey: 'quiz_id',
+  onDelete: 'CASCADE'
+});
+db.QuizOption.belongsTo(db.Quiz, {
+  foreignKey: 'quiz_id',
+  onDelete: 'CASCADE'
+});
+
+db.UserCharacters.hasMany(db.QuizProgress, {
+  foreignKey: 'user_character_id'
+});
+db.QuizProgress.belongsTo(db.UserCharacters, {
+  foreignKey: 'user_character_id'
+});
+
+db.Curriculum.hasMany(db.QuizProgress, {
+  foreignKey: 'curriculum_id'
+});
+db.QuizProgress.belongsTo(db.Curriculum, {
+  foreignKey: 'curriculum_id'
+});
+
+db.Learning.belongsTo(db.Curriculum, {
+  foreignKey: "curriculum_id",
+});
+db.Stories.belongsTo(db.Curriculum, {
+  foreignKey: "curriculum_id",
+});
+
+db.Learning.hasMany(db.LearningProgress, {
+  foreignKey: "learning_id",
+});
+db.LearningProgress.belongsTo(db.Learning, {
+  foreignKey: "learning_id",
+});
+
+db.UserCharacters.hasMany(db.LearningProgress, {
+  foreignKey: "user_character_id",
+});
+db.LearningProgress.belongsTo(db.UserCharacters, {
+  foreignKey: "user_character_id",
+});
+
+db.Stories.hasMany(db.StoryProgress, {
+  foreignKey: "story_id",
+});
+db.Stories.hasMany(db.Scenes, {
+  foreignKey: "story_id",
+});
+db.Scenes.belongsTo(db.Stories, {
+  foreignKey: "story_id",
+});
+
+db.Appear.belongsTo(db.Scenes, {
+  foreignKey: "scene_id",
+});
+db.Appear.belongsTo(db.StoryCharacter, {
+  foreignKey: "character_id",
+});
+
+db.StoryProgress.belongsTo(db.UserCharacters, {
+  foreignKey: 'user_character_id'
+});
+db.UserCharacters.hasMany(db.StoryProgress, {
+  foreignKey: 'user_character_id'
+});
+
+db.StoryProgress.belongsTo(db.Stories, {
+  foreignKey: 'story_id'
+});
+db.Stories.hasMany(db.StoryProgress, {
+  foreignKey: 'story_id'
+});
+
+db.Curriculum.hasOne(db.Learning, {
+  foreignKey: 'curriculum_id'
+});
+db.Curriculum.hasOne(db.Stories, {
+  foreignKey: 'curriculum_id'
+});
+
+db.Scenes.belongsTo(db.Stories, {
+  foreignKey: 'story_id'
+});
+db.Stories.hasMany(db.Scenes, {
+  foreignKey: 'story_id'
+});
+
+db.Scenes.hasMany(db.Dialogues, {
+  foreignKey: 'scene_id'
+});
+db.Dialogues.belongsTo(db.Scenes, {
+  foreignKey: 'scene_id'
+})
+
+db.Scenes.hasMany(db.Choices, {
+  foreignKey: 'scene_id'
+});
+db.Choices.belongsTo(db.Scenes, {
+  foreignKey: 'scene_id'
+});
+
+db.StoryCharacter.belongsToMany(db.Scenes, {
+  through: db.Appear,
+  foreignKey: 'character_id',
+  otherKey: 'scene_id'
+});
+db.Scenes.belongsToMany(db.StoryCharacter, {
+  through: db.Appear,
+  foreignKey: 'scene_id',
+  otherKey: 'character_id'
 });
 
 db.sequelize = sequelize;
---
+++ models/scenes.js
@@ -0,0 +1,29 @@
+// scenes 테이블, scenes.js
+module.exports = (sequelize, DataTypes) =>{
+    const Scenes = sequelize.define("Scenes", {
+      scene_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      background: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+      },
+      story_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'stories',         // FK 
+          key: 'story_id'
+        }
+      }
+    }, {
+      tableName: "scenes",
+      charset: "utf8",
+      collate: "utf8_general_ci",
+      timestamps: false
+    });
+  
+    return Scenes
+    };  
