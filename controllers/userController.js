c33d88c7f44a23385b80062dc1829ce77fd9903b
jwkwon-715 <3r453.618@gmail.com>
2025-05-16 15:32:28 +0900

    회원가입/로그인/로그아웃 구현

---
+++ controllers/userController.js
@@ -0,0 +1,53 @@
+// controllers/userController.js
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const { Users } = require('../models');
+
+// 회원가입 처리_
+
+exports.signup = async (req, res) => {
+  const { user_id, password, password2, phone, email, birthdate } = req.body;
+
+  // 1. 비밀번호 일치 확인
+  if (password !== password2) {
+    return res.send('❌ 비밀번호가 일치하지 않습니다.');
+  }
+
+  try {
+    // 2. 비밀번호 해싱
+    const hashedPassword = await bcrypt.hash(password, 10); // saltRounds = 10
+
+    // 3. 사용자 생성
+    await Users.create({
+      user_id,
+      password: hashedPassword,
+      phone,
+      email,
+      birthdate,
+    });
+
+    // 4. 회원가입 후 로그인 페이지로 이동
+      res.redirect('/users/login?success=1')
+
+
+  } catch (error) {
+    console.error(error);
+    res.send('회원가입 중 오류가 발생했습니다.');
+  }
+};
+
+
+// 로그인 처리
+exports.login = (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return next(err);
+    if (!user) return res.status(401).send(info.message); // 로그인 실패 메시지
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      
+      // 로그인 성공 후 프로필 페이지로 리디렉션
+      return res.redirect('/');  // 로그인 후 메인화면으로
+    });
+  })(req, res, next);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// 회원가입
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+router.post('/signup', userController.signup);
+
+// 로그인
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+router.post('/login', userController.login);
+
+// 로그아웃
+router.get('/logout', (req, res, next) => {
+  req.logout(function(err) {
+    if (err) return next(err);
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
