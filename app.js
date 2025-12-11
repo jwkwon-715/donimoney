d2f7b247e1ffb6506ff7b90aa9ca815fb7e2d496
jinseon901 <100272218+jinseon901@users.noreply.github.com>
2025-12-11 21:15:39 +0900

    Test
    test

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const UserCharacters = db.UserCharacters;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// 기본 미들웨어
+// 기본 미들웨어 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
